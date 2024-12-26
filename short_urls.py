b1cd1e5218a4e408bcee7bf5cb822102baa9ef86
Avakova Anastasiia <avakova.ast@gmail.com>
2024-12-26 23:27:05 +0300

    clean

--- short_urls.py
+++ short_urls.py
@@ -27,10 +27,10 @@ def is_shorten_link(url):
 
         if 'error' in link_info:
             error_message = link_info['error'].get('error_msg', 'Неизвестная ошибка')
-            print(f"Ошибка: {error_message}")
-            return None
+            # return error_message
+            raise Exception(error_message)
         
-        return bool(link_info and link_info.get('response'))
+        return bool(link_info.get('response'))
     return False
 
 def shorten_link(token, url, private=0):
@@ -49,13 +49,13 @@ def shorten_link(token, url, private=0):
 
     if 'error' in link_info:
         error_message = link_info['error'].get('error_msg', 'Неизвестная ошибка')
-        print(f"Ошибка: {error_message}")
-        return None
+        raise Exception(error_message)
     if link_info:
         return link_info['response']['short_url']
     return None
 
 def count_clicks(token, original_url, access_key=None, interval='forever', intervals_count=1, extended=0):
+
     parsed_url = urlparse(original_url)  
     short_url_key = parsed_url.path.lstrip('/')
 
@@ -73,17 +73,14 @@ def count_clicks(token, original_url, access_key=None, interval='forever', inter
     }
     response = requests.get(api_url, params=params)
     response.raise_for_status()  
-
     link_info = response.json()
 
     if 'error' in link_info:
         error_message = link_info['error'].get('error_msg', 'Неизвестная ошибка')
-        print(f"Ошибка: {error_message}")
-        return None
+        raise Exception(error_message)
         
     if link_info:
         stats = link_info['response']['stats']
-        print(f"Статистика по ссылке {original_url}:")
         total_views = sum(stat['views'] for stat in stats)
         return total_views
     return None
@@ -96,8 +93,7 @@ if __name__ == '__main__':
     token = os.environ['VK_TOKEN']
     try:
         if is_shorten_link(original_url):
-            total_views = count_clicks(token, original_url, interval='forever', intervals_count=1, extended=0)
-        
+            total_views = count_clicks(token, original_url, interval='forever', intervals_count=1, extended=0)     
             if total_views is not None:
                 print(f"Общее количество переходов по короткой ссылке: {total_views}")
             else:
@@ -112,3 +108,6 @@ if __name__ == '__main__':
         print(f"Ошибка запроса: {e}")
     except ValueError as e:
         print(f"Ошибка обработки JSON: {e}")
+    except Exception as e:
+        print(f"Ошибка: {e}")
+    
